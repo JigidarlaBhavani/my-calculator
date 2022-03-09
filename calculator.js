e7de750884c6dc4949bb12627c3b818c36ab764b
JigidarlaBhavani <101109993+JigidarlaBhavani@users.noreply.github.com>
2022-03-09 13:13:16 +0530

    uploaded my calculator javascript and html

---
+++ calculator.js
@@ -0,0 +1,10 @@
+function userinput(value){
+    documement.getElementId("userinput").value+=value;
+}function a(){
+    var c=document.getElementById("userinput").value;
+    var d=eval(c);
+    document.getElementById("userinput").value=d;
+}
+
+
+
